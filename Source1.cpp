5286d5882a229e615f95d2f9aac0ad6adc234de1
ahmed hussien <ichbin.ahmed@gmail.com>
2023-03-30 16:35:27 +0200

    the main folder to work on

---
+++ Source1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<Windows.h>
+#include<queue>
+#include<stack>
+#include<set>
+#include<map>
+#include<vector>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	system("color BF");
+	cout << "I'm sorry\n\n\n";
+	
+	return 0;
+}
