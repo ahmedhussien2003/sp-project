60fae8bc8ffaddd479e59724cc9fb3990ae1b675
Raghad <raghooda2003@gmail.com>
2023-06-24 19:37:30 +0300

    thank you

--- SFML.cpp
+++ SFML.cpp
@@ -507,7 +507,7 @@ int main()
 
 void textureAndFonts()
 {
-    //loser
+    //the real loser is you 
     bg1.loadFromFile("C:/Users/hp/source/repos/sfml/Assests/bg1.png");
     bg2.loadFromFile("C:/Users/hp/source/repos/sfml/Assests/bg2.png");
     bg3.loadFromFile("C:/Users/hp/source/repos/sfml/Assests/bg3.png");
